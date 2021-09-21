9ce326e5a1b07d72dded8bac1992df9115d8dcb0
joons5834 <25703159+joons5834@users.noreply.github.com>
2021-09-21 11:27:34 +0000

    pipeline/get_yr_forecast_hourly: add cache header `If-Modified-Since`

--- pipeline/get_yr_forecast_hourly/main.py
+++ pipeline/get_yr_forecast_hourly/main.py
@@ -28,19 +28,26 @@ def get_hourly_forecast_metno_gcs(request):
     bucket = client.get_bucket(bucket_name)
 
     # download the file to Cloud Function's tmp directory
+    
     for lat, lon in POINTS:
         ts_now = datetime.now()
-        headers = {
-            'User-Agent': USER_AGENT   
-        }
+        headers = {'User-Agent': USER_AGENT}
+        last_mod_file_name = folder_name + '/last_modified/' + f'{lat}_{lon}'
+        for blob in client.list_blobs(bucket, prefix=last_mod_file_name):
+            blob = storage.Blob(last_mod_file_name, bucket)
+            headers['If-Modified-Since'] = blob.download_as_bytes().decode('utf-8')
         params = {'lat': lat, 'lon': lon}
         is_response_valid = False
+        is_modified = True
         tries = MAX_TRIES
 
-        while not is_response_valid and tries > 0:
+        while not is_response_valid and tries > 0 and is_modified:
             try:
                 response = requests.get(ENDPOINT, headers=headers, params=params)
                 response.raise_for_status()
+                if response.status_code == 304:
+                    print("Not Modified")
+                    is_modified = False
             except BaseException as e:
                 print(e)
                 print('API error. Retrying')
@@ -52,6 +59,10 @@ def get_hourly_forecast_metno_gcs(request):
         if not is_response_valid:
             raise Exception('API error.')
             
+        if not is_modified:
+            print(f'{lat} {lon} not modified at {ts_now}')
+            continue
+            
 
         file_name = folder_name + '/' + ts_now.strftime(f"metno_hr_fc_{lat}_{lon}_%Y-%m-%d_%H-%M.json")
 
@@ -62,6 +73,11 @@ def get_hourly_forecast_metno_gcs(request):
         blob.upload_from_string(
             data=response.text,
             content_type='application/json'
+        )
+
+        blob = storage.Blob(last_mod_file_name, bucket)
+        blob.upload_from_string(
+            data=response.headers['Last-Modified']
         )
 
     return 'The extract job to gcs is done'
