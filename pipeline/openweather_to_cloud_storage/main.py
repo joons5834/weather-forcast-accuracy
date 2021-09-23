f875e4bd9c2a36401d6fe7a513022234481e4793
joons5834 <25703159+joons5834@users.noreply.github.com>
2021-09-23 11:33:47 +0000

    openweather_to_cloud_storage: delete obsolete comments

--- pipeline/openweather_to_cloud_storage/main.py
+++ pipeline/openweather_to_cloud_storage/main.py
@@ -14,21 +14,16 @@ bucket = client.get_bucket(bucket_name) # get bucket
 
 def import_file(event, context):
     for lat, lon in LOCATIONS:
-        # download the file to Cloud Function's tmp directory
         params = {'lat':lat,'lon':lon,'appid': API_KEY, 'exclude': 'minutely,alerts'}
         r = requests.get(URL, params=params)
         r.raise_for_status()
         t = datetime.utcnow()
         file_name = t.strftime(f"%m-%d-%Y_%H-%M-%S_{lat}_{lon}") + '.json'
-        # cf_path = '/tmp/{}'.format(file_name)
-        # with open(file_name, mode='w') as f:
-        # 	f.write(r.text + '\n')
         
         # set Blob
         blob = storage.Blob(file_name, bucket)
     
-        # upload the file to GCS
-        # blob.upload_from_filename(cf_path)
+        # upload the json to GCS
         blob.upload_from_string(
             data=r.text,
             content_type='application/json'
